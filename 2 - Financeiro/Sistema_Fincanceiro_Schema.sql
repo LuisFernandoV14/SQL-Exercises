640019b39c95541cef00e014986846cfd2150e9f
Luís Fernando Ventura Ferreira <venturaluisf7@outlook.com>
2025-09-13 17:29:50 -0300

    Update Sistema_Fincanceiro_Schema.sql
    Todas tabelas do Sistema prontas. Não fui eu quem fez esse arquivo.
    Blame: Lucas Costa

--- 2 - Financeiro/Sistema_Fincanceiro_Schema.sql
+++ 2 - Financeiro/Sistema_Fincanceiro_Schema.sql
@@ -1,5 +1,4 @@
 -- Vou separar os arquivos em "Schema" (criação e modificação de tabelas), "Data" (inserção, deleção e etc) e "Queries" (consultas no banco de dados)
-
 CREATE DATABASE IF NOT EXISTS sistema_financeiro;
 USE sistema_financeiro;
 
@@ -60,13 +59,39 @@ CREATE TABLE agencia (
 
 CREATE TABLE conta (
 	id_Conta INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    numero_Conta VARCHAR(15),
-    saldo DECIMAL(15,2),
-    data_Abertura DATE,
-    id_Cliente INT,
+    numero_Conta VARCHAR(15) NOT NULL,
+    saldo DECIMAL(15,2) DEFAULT 0.0,
+    data_Abertura DATE DEFAULT CURDATE(),
+    id_Cliente INT NOT NULL,
     
     -- Cliente possui Conta
     CONSTRAINT fk_ClienteConta FOREIGN KEY (id_Cliente) REFERENCES Cliente (id_Cliente)
+    ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE conta_corrente(
+	id_conta INT UNSIGNED PRIMARY KEY,
+    tarifaMensal DECIMAL(5,2) NOT NULL, 
+    
+    CONSTRAINT FK_contaCOR FOREIGN KEY (id_conta) REFERENCES conta(id_conta)
+    ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE conta_poupanca(
+	id_conta INT UNSIGNED PRIMARY KEY,
+    dataRendimento DATE DEFAULT CURDATE(),
+	rendimento DECIMAL(5,2) NOT NULL,
+    
+    CONSTRAINT FK_contaPOU FOREIGN KEY (id_conta) REFERENCES conta(id_conta)
+    ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE conta_investimento(
+	id_conta INT UNSIGNED PRIMARY KEY,
+    tipoInvestimento VARCHAR(63) NOT NULL,
+    valorAplicado DECIMAL(11, 2) NOT NULL,
+    
+    CONSTRAINT FK_contaINV FOREIGN KEY (id_conta) REFERENCES conta(id_conta)
     ON UPDATE CASCADE ON DELETE CASCADE
 );
 
@@ -83,9 +108,38 @@ CREATE TABLE cartao (
 	ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
+CREATE TABLE produtoFinanceiro(
+	id_produtoFinanceiro INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	nomeProduto VARCHAR(127) NOT NULL,
+    descricao VARCHAR(255) NOT NULL,
+    tipoProduto TINYINT UNSIGNED NOT NULL
+);
 
+CREATE TABLE seguro(
+	id_produtoFinanceiro INT UNSIGNED PRIMARY KEY,
+	valorSegurado DECIMAL(11, 2) NOT NULL,
+	tipoCobertura VARCHAR(127) NOT NULL,
+    valorMensalidade DECIMAL(9,2) NOT NULL,
+    
+    CONSTRAINT FK_produtoFinanceiroSEG FOREIGN KEY (id_produtoFinanceiro) REFERENCES produtoFinanceiro(id_produtoFinanceiro)
+    ON UPDATE CASCADE ON DELETE RESTRICT
+);
 
+CREATE TABLE emprestimo(
+	id_produtoFinanceiro INT UNSIGNED PRIMARY KEY,
+    valorEmprestimo DECIMAL(11,2) NOT NULL,
+    juros DECIMAL (6,2) NOT NULL,
+    prazoPagamento DATE NOT NULL,
+    
+    CONSTRAINT FK_produtoFinanceiroEMP FOREIGN KEY (id_produtoFinanceiro) REFERENCES produtoFinanceiro(id_produtoFinanceiro)
+    ON UPDATE CASCADE ON DELETE RESTRICT
+);
 
-
-
-
+CREATE TABLE investimento(
+	id_produtoFinanceiro INT UNSIGNED PRIMARY KEY,
+    taxaRetorno DECIMAL(6,2) NOT NULL,
+    prazoResgate DATE NOT NULL,
+    
+    CONSTRAINT FK_produtoFinanceiroINV FOREIGN KEY (id_produtoFinanceiro) REFERENCES produtoFinanceiro(id_produtoFinanceiro)
+    ON UPDATE CASCADE ON DELETE RESTRICT
+);
